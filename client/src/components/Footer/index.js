f53543680095e368e42679b059fa2941dea680a4
ZekeriyaOtumlu <45694823+ZekeriyaOtumlu@users.noreply.github.com>
2020-02-26 22:18:10 -0500

    backend files updated (#2)

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Footer = () => (
 	<footer className = "page-footer">
 		<div className = "container-fluid text-center">
-			<p>Zekeriya Otumlu Feb 14 2020</p>
+			<p>Zack - Laz - Santi  --- >>> March 2020</p>
 		</div>	
 	</footer>
 );
