a2b976ed4afdecdbdccbcb0876d712ad19d21126
Lazaro <Lazaro.Perez0823@gmail.com>
2020-03-01 18:16:08 -0500

    Working on appending photos - Widget Working with Name

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,32 +3,36 @@ import axios from "axios"
 export default {
 
 
-    // Get book from google search 
+    // Get vacation from google search 
     weatherSearch: function(query) {
         return axios.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + query + "&key=AIzaSyAE2CIuMnHiuUN7XLs9fRiATGN1gD-t0LY")
        },
 
     placeSearch: function(query) {
-      return axios.get("/api/Vacation/placeSearch/" + 'Miami')
+      return axios.get("/api/Vacation/placeSearch/" + query)
      },
 
+     photoSearch: function(query) {
+        return axios.get("/api/Vacation/photoSearch/")
+       },
 
-    // Gets all books
+
+    // Gets all vacations
     // getWeather: function () {
     //     return axios.get("/api/weather");
     // },
 
-    // Gets the book with the given id
+    // Gets the vacation with the given id
     getWeather: function (id) {
         return axios.get("/api/weather/" + id);
     },
 
-    // Saves a book to the database
+    // Saves a vacation to the database
     saveVacation: function (savedVacations) {
         return axios.post("/api/weather", savedVacations);
     },
 
-    // Deletes the book with the given id
+    // Deletes the vacation with the given id
     deleteVacation: function (id) {
         return axios.delete("/api/weather/" + id);
     }
