9bedf6920711b4f3620a1d4ae37844ad7cf3ae7c
Santiago <santyf@live.com>
2020-03-03 18:34:34 -0500

    firebase authentication

--- client/src/config/fire.js
+++ client/src/config/fire.js
@@ -1,15 +1,20 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYSBR6K9CG4m6IuJHWnMifS68F5Jqc6hM",
-    authDomain: "big-o-91712.firebaseapp.com",
-    databaseURL: "https://big-o-91712.firebaseio.com",
-    projectId: "big-o-91712",
-    storageBucket: "big-o-91712.appspot.com",
-    messagingSenderId: "657137252324",
-    appId: "1:657137252324:web:820b9eb8310810d13679f1",
-    measurementId: "G-BK54HX08HD"
-  };
+  apiKey: "AIzaSyDkjznjPuvMbx1Yp1VfIhKUfKk9cS_DpbY",
+  authDomain: "weatherapp-a646d.firebaseapp.com",
+  databaseURL: "https://weatherapp-a646d.firebaseio.com",
+  projectId: "weatherapp-a646d",
+  storageBucket: "weatherapp-a646d.appspot.com",
+  messagingSenderId: "33613602391",
+  appId: "1:33613602391:web:f63acf03e8a9b77aab8d12",
+  measurementId: "G-T5YCT1WEHV"
+};
   
   const fire = firebase.initializeApp(firebaseConfig);
-  export default fire;
+  
+
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  export {fire, googleProvider, facebookProvider}
