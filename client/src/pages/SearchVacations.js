2e09a319c2a082f1739460a5b3abed59f2b69055
LAZARO PEREZ <53586202+LazaroPerez0823@users.noreply.github.com>
2020-03-05 03:06:02 -0500

    Zack030421 (#30)
    * changed
    
    * updated App.js and home.js files

--- client/src/pages/SearchVacations.js
+++ client/src/pages/SearchVacations.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
 import { Container, Row, Col } from "../components/Grid";
-// import SearchForm from "../components/SearchForm";
+import SearchForm from "../components/SearchForm";
 import SearchResult from "../components/SearchResult"
 import './styles.css';
 import { List, ListItem } from "../components/List"
