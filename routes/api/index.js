2efb352e0d32c77498fdc12c651f10b26532a568
LAZARO PEREZ <53586202+LazaroPerez0823@users.noreply.github.com>
2020-03-04 21:40:28 -0500

    Backend completed (#27)

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,20 @@ const router = require("express").Router();
 
 const VacationRoutes = require("./Vacation");
 
+const NotesRoutes = require("./Note");
+
 
 // Book routes
 router.use("/Vacation", VacationRoutes);
 
+router.use("/Note", NotesRoutes)
+
 
 module.exports = router;
+
+
+
+
+
+
+
