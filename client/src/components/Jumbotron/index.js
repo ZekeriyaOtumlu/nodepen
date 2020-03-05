9b9cebed7cc96bd435ba7d6cee32352dffa7831a
Zack <zotumlu78@gmail.com>
2020-03-05 17:17:34 -0500

    updated components

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,31 +1,4 @@
-import React from 'react'
-import "./Form.css"
-export default function Form(props) {
-    return (
-        <form>
-            <div className="form-group">
-                <label className="VacationSearchLabel"></label>
-                <br></br>
-                <input className="col-12 form-control"
-                    value={props.search}
-                    type="text"
-                    name="searchVacation"
-                    placeholder=" Enter Vacation Place"
-                    onChange={props.handleInputChange}
-                />
-            </div>
-            <button type="submit" className="submitBtn btn btn-primary" onClick={props.handleFormSubmit}>
-                Submit
-        </button>
-        </form>
-    )
-}
 
-
-
-
-
-4:58
 import React from "react";
 import "./style.css";
 const SearchForm = props => {
